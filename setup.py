09650b401653ae441cbc6c67646c8a8aafcdd7dd
Alejandro Guarnizo de la Torre <algarcser@gmail.com>
2023-10-27 11:47:10 +0200

    Innitial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is going to be a test run to try see if i can set up a good project',
+    author='alejandro guarnizo de la torre',
+    license='',
+)
